d655382f1e57d781afbcf41f244fe25f5483d516
Mostafa Ehab <mostafaehab@Mostafas-iMac.local>
2020-03-26 23:40:18 +0200

    Change the interface and add Timer function to animate the title label

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -11,10 +11,19 @@ import UIKit
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
-    
+    let titleString = "⚡️FlashChat"
+    var timer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        titleLabel.text = ""
+        var charIndex = 0.0
+        for char in titleString {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(char)
+            }
+            charIndex += 1
+            
+        }
        
     }
     
