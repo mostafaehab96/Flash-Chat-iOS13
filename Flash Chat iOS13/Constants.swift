6df950333db231347504091f54a122b79cd965d2
Mostafa Ehab <mostafaehab@Mostafas-iMac.local>
2020-03-28 14:34:20 +0200

    add Constants file and use it instead of string literals

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Mostafa Ehab on 3/28/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let registerSegue = "RegisterToChat"
+    static let logInSegue = "LoginToChat"
+}
+
+
+
